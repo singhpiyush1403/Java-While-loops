45081d04f7882616409e865deb85fdbb459c3982
PIYUSH SINGH <singhpiyush1403@gmail.com>
2024-11-24 02:21:48 +0530

    Add files via upload

---
+++ Main (5).java
@@ -0,0 +1,31 @@
+
+//
+//public class Main {
+//
+//	public static void main(String[] args) {
+//		int i = 0;
+//		while(i<6) {
+//			System.out.println(i);
+//			i++;
+//		}
+//	}
+//}
+
+
+
+import java.util.Scanner;
+
+public class Main{
+
+public static void main(String args []) {
+	
+	Scanner sc = new Scanner(System.in);
+	Boolean learn = false;
+	while(learn) {
+		System.out.println("went to school , try to learn");
+		learn =  sc.nextBoolean();
+		
+	}
+}
+	
+}
